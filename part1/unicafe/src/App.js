13c53a7440d7b554adf100ba25515ec82707990d
Mudit Mahajan <muditmahajan21@gmail.com>
2022-02-08 00:46:04 +0530

    part1 exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,28 +1,25 @@
 import React, { useState } from 'react'
 
-const TotalStat = (props) => {
+const Statistics = ({good, neutral, bad, all}) => {
+  if(all === 0) {
+    return (
+      <>
+        <p> No feedback given </p>
+      </>
+    )
+  }
+  
+  const average = (good - bad) / all
+  const positive = good / all
   return (
-    <>
-      <p> all {props.good + props.neutral + props.bad} </p>
-    </>
-  )
-} 
-
-const AverageStat = (props) => {
-  const avg = (props.good - props.bad) / (props.good + props.neutral + props.bad)
-  return (
-    <>
-      <p> average {avg} </p>
-    </>
-  )
-} 
-
-const Percentage = (props) => {
-  const percent = (props.good) / (props.good + props.neutral + props.bad)
-  return (
-    <>
-      <p> positive {percent} % </p>
-    </>
+    <p>
+      good {good} <br />
+      neutral {neutral} <br /> 
+      bad {bad} <br />
+      all {all} <br /> 
+      average {average} <br />
+      positive {positive} % <br />
+    </p>
   )
 }
 
@@ -31,35 +28,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [all, setAll] = useState(0)
+  
   const changeGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const changeNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const changeBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
     <>
       <h1>give feedback</h1>
-      
+
       <button onClick={changeGood} > good </button>
       <button onClick={changeNeutral} > neutral </button>
       <button onClick={changeBad} > bad </button>
 
       <h3>statistics</h3>
-      <p> good {good} </p>
-      <p> neutral {neutral} </p>
-      <p> bad {bad} </p>
-
-      <TotalStat good = {good} neutral = {neutral} bad = {bad} />
-      <AverageStat good = {good} neutral = {neutral} bad = {bad} />
-      <Percentage good = {good} neutral = {neutral} bad = {bad}/>
+      
+      <Statistics good = {good} neutral = {neutral} bad = {bad} all = {good + neutral + bad} />
     </>
   )
 }
