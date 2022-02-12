6c4b2719872ec031d636f71be4e89c57d603097d
Mudit Mahajan <muditmahajan21@gmail.com>
2022-02-12 18:50:14 +0530

    part2 exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+
+import axios from 'axios'
+
 const Heading = ( {heading} ) => <h2> {heading} </h2>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
   const [filter, setFilter] = useState('')
   const [filterPersons, setFilterPersons] = useState(persons)
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+  
   const addPerson = (event) => {
     event.preventDefault()
 
