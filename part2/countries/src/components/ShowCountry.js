ad040fd40894466bc3f05a581b83378161c827b2
Mudit Mahajan <muditmahajan21@gmail.com>
2022-02-13 02:22:54 +0530

    part2 exercise 2.14

--- part2/countries/src/components/ShowCountry.js
+++ part2/countries/src/components/ShowCountry.js
@@ -1,7 +1,24 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const ShowCountry = ({country}) => {
     const [showView, setShowView] = useState(false)
+    const [currentWeather, setCurrentWeather] = useState({})
+
+    const apiKey = process.env.REACT_APP_API_KEY
+
+    useEffect(() => {
+        axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${apiKey}`)
+        .then(response => {
+            const currentWeatherObject = {
+                temp: response.data.main.temp,
+                img: 'http://openweathermap.org/img/wn/' + response.data.weather.icon + '@2x.png',
+                wind: response.data.wind.speed
+            }
+            setCurrentWeather(currentWeatherObject)
+        })
+    })
 
     const renderView = showView ? 
     <li>
@@ -14,6 +31,12 @@ const ShowCountry = ({country}) => {
         </ul>
       </div>
         <div style={{ fontSize: '20em' }}>{country.flag} </div>
+    <div>
+        <h2> Weather in {country.capital} </h2>
+        <strong> Temperature: </strong> {currentWeather.temp} Celsius <br />
+        <img src={currentWeather.img} alt={country.capital} />
+        <strong> Wind: </strong> {currentWeather.wind}    
+    </div>
       <button onClick={() => setShowView(!showView)}> {showView ? 'Hide' : 'Show'} </button>
     </li>
     : <li>
