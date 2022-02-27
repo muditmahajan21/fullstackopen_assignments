e8f3150fa124051558dbb5e21473cbbc86ae4521
Mudit Mahajan <muditmahajan21@gmail.com>
2022-02-28 01:01:58 +0530

    part4 exercise 4.4

--- part4/blgList/utils/list_helper.js
+++ part4/blgList/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = {
-    dummy
+const totalLikes = (blogs) => {
+    if(!Array.isArray(blogs))
+        return 0
+
+    if(blogs.length === 0) 
+        return 0
+    
+    if(blogs.length === 1) 
+        return blogs[0].likes
+    
+    const total = blogs.reduce((tot, blog) => tot + blog.likes, 0)
+    return total
 }
+
+module.exports = {
+    dummy, totalLikes
+}
+
