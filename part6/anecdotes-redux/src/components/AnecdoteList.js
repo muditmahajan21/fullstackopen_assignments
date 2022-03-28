9705a3f90ccbd187648dda9befc42ff298899dcc
Mudit Mahajan <muditmahajan21@gmail.com>
2022-03-28 14:46:03 +0530

    part6 exercise 6.10-6.11

--- part6/anecdotes-redux/src/components/AnecdoteList.js
+++ part6/anecdotes-redux/src/components/AnecdoteList.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
+import { createNotification } from "../reducers/notificationReducer"
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => state.anecdotes)
     const dispatch = useDispatch()
-    
-    const vote = (id) => {
-        console.log('vote', id)
-        dispatch(voteAnecdote(id))
+
+    const vote = (anecdote) => {
+        console.log('vote', anecdote.id)
+        dispatch(voteAnecdote(anecdote.id))
+        dispatch(createNotification(`You voted for "${anecdote.content}"`))
+        setTimeout(() => {
+            dispatch(createNotification(null))
+        }
+        , 5000)
     }
     
     return (
@@ -21,7 +27,7 @@ const AnecdoteList = () => {
             </div>
             <div>
                 has {anecdote.votes}
-                <button onClick={() => vote(anecdote.id)}>vote</button>
+                <button onClick={() => vote(anecdote)}>vote</button>
             </div>
             </div>
         )}
